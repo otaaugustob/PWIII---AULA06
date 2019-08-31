49d1dbd01a5f213b0af5e15b19d2fe80c40c880e
Otavio <otavioabottin@outlook.com>
2019-08-30 21:50:13 -0300

    Data Binding Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'etec-base';
+  peso = 90;
+  altura = 1.78;
+
+  exibir() : void{
+  alert("O IMC é :" + this.peso / (this.altura*this.altura));
+
+  /*let imc = this.peso /(this.altura*this.altura);
+  alert("O imc é " + imc)
+  */
+  }
 }
